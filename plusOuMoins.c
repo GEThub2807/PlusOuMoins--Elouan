7ad9c8225020ddc63420e430d5a274f3d846ce1d
E.T <e.thepaut28@gmail.com>
2024-03-11 16:08:01 +0100

    Update

--- plusOuMoins.c
+++ plusOuMoins.c
@@ -1,19 +1,19 @@
-#include <stdbool.h>
+/* #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-unsigned int MaxNumber= 0;
-unsigned int MinNumber= 0;
+int MaxNumber= 0;
+int MinNumber= 0;
 
 int AskInt(){
-    unsigned int number;
+    int number;
     while(scanf(" %u", &number) != 1){
-        printf("\nEntrez un nombre positif entier entre %u et %u: ", MinNumber, MaxNumber);
+        printf("\nEntrez un nombre positif entier entre %d et %d: ", MinNumber, MaxNumber);
     }
 }
 
-unsigned int AskIntInRange(uint min, uint  max){
+int AskIntInRange(int min, int  max){
     unsigned int number;
     while(true){
         number= AskInt();
@@ -37,9 +37,9 @@ int main() {
     Max = AskIntInRange();
 
     while( true ) {
-        unsigned int UserChoice;
-        unsigned int searchedValue = MinNumber + rand() % (MaxNumber + 1); //remplacer les var par 101
-        unsigned int Try = 3;
+        int UserChoice;
+        int searchedValue = MinNumber + rand() % (MaxNumber + 1); //remplacer les var par 101
+        int Try = 3;
         printf( " Débug: Valeur secrète == %d\n", searchedValue );
 
         while( true ) {
@@ -65,6 +65,98 @@ int main() {
 
         printf( "Souhaitez-vous recommencer une partie (0 Non / 1 Oui) : " );
         fflush( stdout );
+        scanf( "%d", & editedValue);
+        if ( editedValue == 0 ) break;
+    }
+
+    printf( "Bye bye !\n" );
+
+    return EXIT_SUCCESS;
+}*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int AskInt(){
+    int number;
+    int numberError = scanf_s("%d", &number);
+
+    while (number =! 1){
+        if (numberError == 0){
+            printf("Veuillez rentrer un nombre entier positif");
+            return 1;
+        }else{
+            return number;
+        }
+    }
+}
+
+int AskIntInRange(int min, int  max){
+    int number;
+    while(1){
+        number= AskInt();
+        if  (number >= min && number <= max){
+            break;
+        }
+        printf("Veuillez inscrire un nombre entre %u et %u: \n" ,min, max);
+    }
+    return number;
+}
+
+char Replay(){
+    char replay[256];
+    int rError = scanf_s("%s", replay, 256);
+
+    if (rError == 0 || (replay != 'y' && 'n' && 'Y' && 'N')){
+        printf("Veuillez rentrer une réponse valide : y ou n \n");
+        return 1;
+    }else{
+        return replay;
+    }
+}
+
+
+int main() {
+
+    srand( time( NULL ) );
+
+    while( 1 ) {
+        /*printf("Quel sera le plus petit numéro ?");
+        int min;
+        min = AskIntInRange();
+
+        printf("Quel sera le plus grand numéro ?");
+        int max;
+        max = AskIntInRange();*/
+        int min = 0;
+        int max = 100;
+        int searchedValue = (rand() * (max - min) / RAND_MAX) + min;
+        int editedValue;
+        int Essais = 3;
+        printf("Valeur à trouver == %d\n", searchedValue );
+
+        while(1) {
+            printf( "Veuillez saisir un entier (entre 0 et 100) : " );
+            editedValue = AskInt();
+            Essais--;
+
+            if ( editedValue == searchedValue ) {
+                printf( "Félicitation : il vous restait %d !\n", Essais );
+                break;
+            }
+            if ( editedValue < searchedValue ) {
+                printf( "La valeur à trouver est plus grande.\n" );
+            } else {
+                printf( "La valeur à trouver est plus petite.\n" );
+            }
+            if (Essais == 0){
+                printf("Vous aviez 3 essais, dommage, le nombre recherché était %u .", searchedValue);
+                break;
+            }
+        }
+
+        printf( "Souhaitez-vous recommencer une partie (0 Non / 1 Oui) : " );
         scanf( "%d", & editedValue );
         if ( editedValue == 0 ) break;
     }
@@ -73,50 +165,3 @@ int main() {
 
     return EXIT_SUCCESS;
 }
-
-/*
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-
-int main() {
-
-    // On commence par initialiser le générateur de nombre pseudo-aléatoires.
-    srand( time( NULL ) );
-
-    while( true ) {
-        // Tirer une valeur aléatoire entre 0 et 100.
-        int searchedValue = rand() % 101;       // % => Reste de la division entière
-        int editedValue;
-        int score = 0;
-        printf( "MegaGame V1.0 - Valeur secrète == %d\n", searchedValue );
-
-        while( true ) {
-            printf( "Veuillez saisir un entier (entre 0 et 100) : " );
-            fflush( stdout );
-            scanf( "%d", & editedValue );
-            score++;
-
-            if ( editedValue == searchedValue ) {
-                printf( "Félicitation : trouvé en %d coups !\n", score );
-                break;      // intérrompre la boucle des manches (pas celle des parties)
-            }
-            if ( editedValue < searchedValue ) {
-                printf( "La valeur à trouver est plus grande.\n" );
-            } else {
-                printf( "La valeur à trouver est plus petite.\n" );
-            }
-        }
-
-        printf( "Souhaitez-vous recommencer une partie (0 Non / 1 Oui) : " );
-        fflush( stdout );
-        scanf( "%d", & editedValue );
-        if ( editedValue == 0 ) break;
-    }
-
-    printf( "Bye bye !\n" );
-
-    return EXIT_SUCCESS;
-}*/
